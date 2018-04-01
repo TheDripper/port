42bb29354beb7a6df42591eff3248c0ccaca8c68
TheDripper <tylerehill@gmail.com>
2018-03-31 18:23:17 -0700

    novid

---
+++ port.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',e=>{
+	let flash = document.getElementById('flash');
+	let splash = document.getElementById('splash');
+	flash.onclick = function(e) {
+		console.log('test');
+		splash.src='night.mp4';
+	}
+});
